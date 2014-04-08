f4fa8442db396f46d0ba5f96a32402106010fea4
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-04-08 12:52:05 +0200

    age in days

--- 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
+++ 01-Ruby/01-Programming-basics/03-Age-in-days/lib/age_in_days.rb
@@ -4,5 +4,6 @@ require 'date'
 
 
 def age_in_days(day, month, year)
-  #TODO: return the age expressed in days given the day, month, and year of birth
+  age = Date.today - Date.new(day, month, year)
+  age.to_i
 end
