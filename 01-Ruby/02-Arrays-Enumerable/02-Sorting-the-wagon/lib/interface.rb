0e5a57cfc8010ef5bb37a0876d250d7f15868f7f
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-04-09 15:42:24 +0200

    at last I made it

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -9,6 +9,22 @@ begin
 
   # TODO: Add the name we just gotto the students array
 
+  students.push(name)
+
 end while name != ""
 
 # TODO: Call `wagon_sort` method and display the sorted student list
+
+new_students = wagon_sort(students)
+
+puts "Congratulations! Your Wagon has #{students.count - 1 } students:"
+print "- "
+
+print new_students[1]
+
+  for x in (2...(new_students.length - 1))
+    print ", " + new_students[x]
+  end
+
+print " and #{new_students.last}"
+
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,3 +1,9 @@
 def wagon_sort(students)
   # TODO: return a copy of students, sorted alphabetically
+  sorted_wagon = students.sort_by{|word| word.downcase}
+
+return sorted_wagon
+
 end
+
+
