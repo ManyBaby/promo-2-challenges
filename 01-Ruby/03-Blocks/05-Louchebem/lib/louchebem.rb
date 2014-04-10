c9529cbf695cd46520015ac06a1eacba9fc00380
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-04-10 16:25:42 +0200

    first attempt

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -2,4 +2,31 @@
 
 def louchebemize(sentence)
   #TODO: implement your louchebem translator
+word_array = sentence.split(" ")
+
+suffixes = ["em", "é", "ji", "oc", "ic", "uche", "ès"]
+suffix = suffixes.shuffle.first
+
+
+  word_array.each do |word|
+
+    if word.length == 1
+      word = word
+    elsif word.include?("!")
+      word = word
+    elsif word.include?("?")
+      word = word
+    elsif word.include?(":")
+      word = word
+    elsif word.include?(",")
+      word = word
+    elsif word[0].include?["a", "e", "i", "o", "u", "y"]
+      word = "l" + word + suffix
+    else
+      word = word + suffix
+    end
+  end
+
+  print word_array.join(" ")
+
 end
