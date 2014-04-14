2e03e511b90dd1ab2e694fcc385a027f26386f13
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-04-14 14:06:56 +0200

    jskngd

--- 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
+++ 01-Ruby/05-Regular-Expressions/01-Phone-regexp/lib/check_phone.rb
@@ -1,3 +1,10 @@
 def french_phone_number?(phone_number)
   # TODO: true or false?
+  phone_number_clean = phone_number.delete('')
+  phone_number_clean.delete!('-')
+  if phone_number_clean =~ /(0|\+33)\s?([1-9])(\s?\d{2}){4}/
+    return true
+  else
+    return false
+  end
 end
