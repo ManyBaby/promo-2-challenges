6822a20d51aefce14116a6fc4e0eb487deb6fcb3
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-04-16 17:17:08 +0200

    fail

--- 02-OOP/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/03-Basic-OOP/lib/orange_tree.rb
@@ -3,30 +3,59 @@ class OrangeTree
   def initialize
     @age = 0
     @dead = false
-    @size = 0
+    @height = 0
     @fruits = 0
   end
 
-  def one_more_year
-    @age += 1
-    @size += 1 if @age <= 10
-    if @age >= 5
-      @fruits += 100
-    elsif @age >= 10
-      @fruits += 200
-    elsif @age >= 15
-      @fruits
-
+  def produce_fruits
+    @fruits = 0
+    if @age > 5 && @age < 11
+      @fruits = 100
+    elsif @age > 10 && @age < 16
+      @fruits = 200
+    elsif @age > 15
+      @fruits = 0
+    else
     end
   end
 
-  def pick_fruit
+  def getting_older
+    @age += 1
+  end
+
+  def getting_taller
+    @height += 1 if @age <= 10
+  end
+
+  def one_year_passes!
+    getting_older()
+    getting_taller()
+    produce_fruits()
+    random
+  end
+
+  def random
+    if @age < 50
+      @dead = false
+    elsif @age > 49 && @age < 101
+      if random(10) < 5
+        @dead = true
+      else
+        @dead = false
+      end
+    else
+      @dead = true
+    end
+  end
+
+  def dead?
+    return @dead
+  end
+
+  def pick_a_fruit!
     @fruits -= 1
   end
 
-  def kill
-
-
-  end
+  attr_reader :dead, :height, :fruits, :age
 
 end
