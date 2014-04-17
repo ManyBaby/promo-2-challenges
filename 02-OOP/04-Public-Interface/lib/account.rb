d447ac30dea513ec65b18ee02a56df9cfadb2aab
Emmanuel Louisy-Gabriel <emmanuel@skycitizen.fr>
2014-04-17 16:47:34 +0200

    at last

--- 02-OOP/04-Public-Interface/lib/account.rb
+++ 02-OOP/04-Public-Interface/lib/account.rb
@@ -12,6 +12,9 @@ class BankAccount
   # - you can withdraw or deposit money
   # - You can see the balance of the account (through the position variable)
 
+
+  attr_reader :position, :name, :hidden_iban
+
   MIN_DEPOSIT =  100
 
   def initialize(name, iban, initial_deposit, password)
@@ -26,33 +29,80 @@ class BankAccount
 
   def withdraw(amount)
     # TODO: Call add_transaction with the right argument
+    add_transaction(-amount)
     # TODO: returns a string with a message
+    "You've just withdrawn #{amount} euros"
   end
 
   def deposit(amount)
     # TODO: Call add_transaction with the right argument
+    add_transaction(amount)
     # TODO: returns a string with a message
+    "You've just deposit #{amount} euros"
   end
 
   def transactions_history(args = {})
     # TODO: Check if there is a password and if so if it is correct
+      transaction_safe = @transactions.join
+    if args[:password] == @password
+      transaction_safe
+    elsif args.empty?
+      'no password given'
+    else
+      'wrong password'
+    end
     # TODO: return a string displaying the transactions, BUT NOT return the transaction array !
+
   end
 
   def iban
     # TODO: Hide the middle of the IBAN like FR14**************606 and return it
+    @hidden_iban = @iban
+    @hidden_iban[4..-4] = "**************"
+    @hidden_iban
+
   end
 
   def to_s
     # Method used when printing account object as string (also used for string interpolation)
     # TODO: Displays the account owner, the hidden iban and the position of the account
+    "Owner : #{@name}, IBAN : #{iban}, Current amount : #{@position} euros"
   end
 
+
+########################
   private
+
+
 
   def add_transaction(amount)
     # TODO: add the amount in the transactions array
+    @transactions << amount
     # TODO: update the current position (which represents the balance of the account)
+    @position += amount
   end
 
 end
+
+
+
+account = BankAccount.new("Manu", "7767FR7777", 120, "pswd")
+
+puts account.withdraw(20)
+
+puts account.deposit(50)
+
+puts account.position
+
+puts account # account.to_s
+
+puts account.transactions_history
+
+puts account.transactions_history({password: "badpassword"})
+
+puts account.transactions_history({password: "pswd"})
+
+
+
+
+
